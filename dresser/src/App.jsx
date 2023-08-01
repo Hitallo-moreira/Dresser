35dd96e063ce51a831914f97851f8eb6e8a83bc2
Hitallo-moreira <hitallomoreira13@gmail.com>
2023-08-01 16:25:16 -0300

    feat: add components in app

--- dresser/src/App.jsx
+++ dresser/src/App.jsx
@@ -1,5 +1,8 @@
 import Header from "./components/Header"
 import HeroSection from "./components/HeroSection";
+import Brands from "./components/Brands";
+import FeaturedProducts from "./components/FeaturedProducts";
+import Categories from "./components/CategoriesSection";
 
 function App() {
   const menuItems = ['Feminino', 'Masculino', 'Marcas'];
@@ -8,6 +11,9 @@ function App() {
     <div className="container">
       <Header menuItems={menuItems} />
       <HeroSection />
+      <Brands />
+      <FeaturedProducts />
+      <Categories />
     </div>
   )
 }
