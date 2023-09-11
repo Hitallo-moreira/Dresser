1881b370b443c8153dd7d0b46bfd92f17dd1fc1e
Hitallo-moreira <hitallomoreira13@gmail.com>
2023-09-11 15:16:28 -0300

    feat: add router

--- dresser/src/main.jsx
+++ dresser/src/main.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import './styles/styles.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import App from './App';
+import HomePage from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import CreateAccount from "./pages/CreateAccount.jsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App>
+      <HomePage />
+    </App>,
+  },
+  {
+    path: "login",
+    element: <Login />, 
+  },
+  {
+    path: "criarconta",
+    element: <CreateAccount />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
